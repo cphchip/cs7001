42b3c129cd670ef1d48e03c90e983e60144d988e
Chip-PC <cphchip@gmail.com>
2022-02-27 15:16:52 -0600

    Updates from PC

---
+++ Assignment_1.7.py
@@ -0,0 +1,15 @@
+﻿userList = ['foo', 'bar', 99.3]
+
+# for i in userList:
+#     if type(userList[i]) != str:
+#         userList[i] = str(userList[i])
+
+
+# concOutput = userList[0] + userList[1] + userList[2]
+
+print (type(userList[1]))
+
+# print (concOutput)
+
+
+

37864a522c649db9c55278e304fec1971d8b2fe3
Chip <cphchip@gmail.com>
2022-03-03 17:00:23 -0600

    Finish 1.7 and start 1.8 from surface

--- Assignment_1.7.py
+++ Assignment_1.7.py
@@ -1,12 +1,17 @@
-﻿userList = ['foo', 'bar', 99.3]
-strOut = ""
+﻿from typing import Any
 
-for i in userList:
-    if type(i) == str:
-        strTemp = i
-    else:
-        i = str(i)
-        strTemp = i
-    strOut = strOut + strTemp
 
-print(strOut)
+userList = ['foo', 'bar', False, 99.3, True]
+
+def make_strl(mylist:list) -> str:
+    strOut = ""
+    for i in mylist:
+        if type(i) == str:
+            strTemp = i
+        else:
+            i = str(i)
+            strTemp = i
+        strOut = strOut + strTemp
+    return strOut
+
+print(make_strl(userList))
---
+++ Assigntment_1.8.py
@@ -0,0 +1,7 @@
+# 8. Write a Python function to print out a set containing all the colors from color_list_1 which are not
+# present in color_list_2. Display result after function returns.
+# function: def extract1(color_list_1 : List[str], color_list_2: List[str] -> set:
+# IN: 
+#   color_list_1 = ["White", "Black", "Red"]
+#   pwdcolor_list_2 = ["Red", "Green"]
+# OUT: {'Black', 'White'}

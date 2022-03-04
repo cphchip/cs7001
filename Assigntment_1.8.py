681c8f0bf09dc862eb220057718357b952131820
Chip <cphchip@gmail.com>
2022-03-04 10:15:02 -0600

    Updates for assignment 8 from surface

--- Assigntment_1.8.py
+++ Assigntment_1.8.py
@@ -10,22 +10,30 @@
 # Python doesn't seem to like List[str], but list is ok
 
 from ast import And
-from asyncio.windows_events import NULL
 
 
 userList1 = ["White", "Black", "Red"]
 userList2 = ["Red", "Green"]
+
+# testStr = "goat"
+# myTestSet = set()
+# myTestSet.add(testStr)
+# # print(testStr in myTestSet)
+# bCheck = testStr in myTestSet
+
+# if testStr in myTestSet == False:
+#     print("yes")
+# else:
+#     print('no')
 
 def extract1(color_list_1 : list, color_list_2: list) -> set:
     mySet = set()
     for i in color_list_1:
         for x in color_list_2:
             if i != x: 
-                if i in mySet == False:
+                # if i in mySet == False or len(mySet) == 0:  First condition doesn't work with this syntax, why?
+                if i not in mySet or len(mySet) == 0: # This one works fine???
                     mySet.add(i)
-                else:
-                    if len(mySet) == 0:
-                        mySet.add(i)
     return mySet
 
 print(extract1(userList1, userList2))

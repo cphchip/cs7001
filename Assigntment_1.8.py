06957d8884b74d4aa9c92c9b0628f0f04af59f6e
Chip <cphchip@gmail.com>
2022-03-04 06:45:12 -0600

    Assignment 8 updates from surface

--- Assigntment_1.8.py
+++ Assigntment_1.8.py
@@ -5,3 +5,27 @@
 #   color_list_1 = ["White", "Black", "Red"]
 #   pwdcolor_list_2 = ["Red", "Green"]
 # OUT: {'Black', 'White'}
+
+# Note I have trouble with the syntax on some of these assignments
+# Python doesn't seem to like List[str], but list is ok
+
+from ast import And
+from asyncio.windows_events import NULL
+
+
+userList1 = ["White", "Black", "Red"]
+userList2 = ["Red", "Green"]
+
+def extract1(color_list_1 : list, color_list_2: list) -> set:
+    mySet = set()
+    for i in color_list_1:
+        for x in color_list_2:
+            if i != x: 
+                if i in mySet == False:
+                    mySet.add(i)
+                else:
+                    if len(mySet) == 0:
+                        mySet.add(i)
+    return mySet
+
+print(extract1(userList1, userList2))

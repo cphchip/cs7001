24404ee06d97bfc5c06572e03191a0f33b8eecec
Chip <cphchip@gmail.com>
2022-04-16 07:24:58 -0500

    working 3.5

--- Assignment 3.5.py
+++ Assignment 3.5.py
@@ -8,3 +8,21 @@
 # IN: {'x': '10.12', 'y': '20.23', 'z': '30'}, {'p': '40.00', 'q': '50.19', 'r': '60'}
 # OUT: {'x': 10.12, 'y': 20.23, 'z': 30.0}, {'p': 40.0, 'q': 50.19, 'r': 60}
 
+dict1 = {'x': '10', 'y': '20', 'z': '30'}, {'p': '40', 'q': '50', 'r': '60'}
+# dict1 = {'x': '10.12', 'y': '20.23', 'z': '30'}, {'p': '40.00', 'q': '50.19', 'r': '60'}
+
+for d in dict1:
+    for value in d:
+        if type(d[value]) == str:
+            d[value] = int(d[value])
+        # if type(d[value])
+
+        # if type(d[value]) == str:
+        #     if isinstance(d[value], int):
+        #         d[value] = int(d[value])
+        #     elif isinstance(d[value], float):
+        #         d[value] = float(d[value])
+    print (dict1)
+
+# print (type(dict1))
+# print (dict1)

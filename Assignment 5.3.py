96976f9b45ff9c2a6a1e3b251746c6a974b0b253
Chip <cphchip@gmail.com>
2022-04-24 07:00:54 -0500

    completed 5.3

--- Assignment 5.3.py
+++ Assignment 5.3.py
@@ -4,3 +4,16 @@
 # If n2 is passed as zero, catch the exception, display "Can't divide by Zero" and return None as the value 
 # of the function.
 # Do not use if statements – use Exceptions!
+
+from typing import Any
+
+def divide (n1: int, n2: int) -> Any:
+
+    try:
+        result = n1 / n2
+        return result
+    except ZeroDivisionError:
+        print("Can't devide by Zero")
+        return "None"
+
+print(divide (12, 0))

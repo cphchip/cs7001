d475ee17001b223a82befcca1f201f34cb11402b
Chip-PC <cphchip@gmail.com>
2022-05-06 12:04:04 -0500

    Check of 1.1 and 1.2 done

--- Assignment_1.1.py
+++ Assignment_1.1.py
@@ -1,9 +1,18 @@
+# Assignment 1.1
+# 
+# Write a Python function which accepts the radius of a circle and returns the area. Display area after 
+# function returns.
+# function: def get_area(radius : float)-> float:
+# Input: 1.1
+# Output: 3.8013271108436504
+
 import math
 
 def get_area(radius : float) -> str:
     return radius ** 2 * math.pi
 
+# Ask User to input radius
 radius_val = float(input("Enter the radius: "))
-area = get_area(radius_val)
-print("The area is: ")
-print(area)
+
+# Get the area by running function with user input and display 
+print("The area is: ", get_area(radius_val))
--- Assignment_1.2.py
+++ Assignment_1.2.py
@@ -1,12 +1,21 @@
+# Assignment 1.2
+# 
+# Write a Python function which accepts a list of strings and returns the last string in the list. Display
+# the last string after function returns.
+# function: def get_last(mylist : list[str]) -> str:
+# IN: ['foo', 'bar', 'zork']
+# OUT: 'zork
 
+
+# Get strings input by the user
 str1 = str(input("Enter first string: "))
 str2 = str(input("Enter second string: "))
 str3 = str(input("Enter third string: "))
 
-userList = [str1, str2, str3]
-listCount =  len(userList)
+userList = [str1, str2, str3] # Create a list of the strings
+print(len(userList))
 
 def get_last(mylist : list[str]) -> str:
-     return mylist[listCount-1]
+     return mylist[len(mylist)-1]  # Using the length of the list return the last value accounting for 0 index
 
-print (get_last(userList))
+print (get_last(userList)) # Print result using function

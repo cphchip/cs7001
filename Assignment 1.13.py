489ea44f6732c61b5e0a2296c36376521a3eca58
Chip <cphchip@gmail.com>
2022-05-27 09:40:22 -0500

    checked through 1.14

--- Assignment 1.13.py
+++ Assignment 1.13.py
@@ -13,16 +13,17 @@
     
     # Input: [1, 2, 2, 3, 3, 3]
     # Output: True
-   
+
+# Variable for test cases
 ithList = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6]
 
 def is_ith(p1 : list) -> bool:
-    bList = []
-    for i in ithList:
-        if ithList.count(i) == i:
+    bList = []  # Create an empty list that will hold bool values
+    for i in ithList: # Loop through each value in the test case
+        if ithList.count(i) == i: # Count quantity of occurrences of value and check if it matches value
             bList.append(True)
         else:
             bList.append(False)
-    return all(bList)
+    return all(bList) #Use all() function to verify if every value in bList is true to return correct result
 
-print(is_ith(ithList))
+print(is_ith(ithList)) # Call function in print statement
--- Assignment 1.14.py
+++ Assignment 1.14.py
@@ -12,14 +12,14 @@
 #     Input: ['cat', 'dog', 'shatter', 'donut', 'at', 'todo', '']
 #     Output: shatter
 
-def longest_str (P1: list) -> str:
-    longestWordLength = 0
-    for i in wordList:
-        if len(i) > longestWordLength:
+def longest_str (p1: list) -> str:
+    longestWordLength = 0 # Create 0 value variable to compare against which ensures first word will orver-write
+    for i in p1: # Loop through each value provided in test cases
+        if len(i) > longestWordLength: # if value is longer than check variable, replace with this value
             longestWord = i
             longestWordLength = len(i)
     return longestWord
 
-wordList = ['cat', 'dog', 'shatter', 'donut', 'at', 'todo', '']
+wordList = ['cat', 'dog', 'shatter', 'donut', 'at', 'todo', ''] # Provided test case
 
-print(longest_str(wordList))
+print(longest_str(wordList)) # Call function in print statement
